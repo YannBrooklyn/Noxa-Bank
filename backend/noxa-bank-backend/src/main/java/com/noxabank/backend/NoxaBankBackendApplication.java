396783f8a59657ea65e4dd9b83ff76ecedf20e93
YannBrooklyn <yann.massoro@gmail.com>
2026-05-09 11:45:58 +0200

    chore: initialize noxa-bank foundation

---
+++ backend/noxa-bank-backend/src/main/java/com/noxabank/backend/NoxaBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.noxabank.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoxaBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoxaBankBackendApplication.class, args);
+	}
+
+}
---
+++ backend/noxa-bank-backend/src/test/java/com/noxabank/backend/NoxaBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noxabank.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoxaBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
